25423987811ccdc67c8e2b7368f1eea8deaaf955
Julierme Peixoto <julierme.peixoto@monitoratec.com.br>
2021-02-24 13:59:24 -0300

    Ciclo de vida dos objetos

---
+++ src/dados/Categorias.js
@@ -0,0 +1,25 @@
+export default class Categorias{
+    constructor(){
+        this.categorias = [];
+        this._inscritos = [];
+    }
+
+    inscrever(func){
+        this._inscritos.push(func);
+    }
+
+    notificar(){
+        this._inscritos.forEach(func =>{
+            func(this.categorias);
+        } );
+    }
+
+    desinscrever(func){
+        this._inscritos = this._inscritos.filter(f => f !== func);
+    }
+
+    adicionarCategoria(novaCaegoria){        
+        this.categorias.push(novaCaegoria);
+        this.notificar();
+    }
+}
